841efccb21a5f59783f51d272cc5b49b4d59cb9f
aleksej <man747704@gmail.com>
2024-12-05 04:47:53 +0300

    feat(refactor): add logging stud

--- src/main/java/com/modsen/software/passenger/kafka/consumer/RideConsumer.java
+++ src/main/java/com/modsen/software/passenger/kafka/consumer/RideConsumer.java
@@ -6,12 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 @RequiredArgsConstructor
 public class RideConsumer {
 
     @KafkaListener(topics = KafkaTopics.RIDES_TOPIC, groupId = "passengerConsumerGroup")
     public void listenRides(BaseRideEvent rideEvent) {
-        System.out.println(rideEvent);
+        Logger.getGlobal().info(rideEvent.toString());
     }
 }
